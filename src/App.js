790b8640da288e99e670e559ca90c8d4b0d6d4ed
Olivia Rivas~ <olivia.rivas.prf@gmail.com>
2023-11-29 10:44:14 -0600

    counter

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
-import logo from './logo.svg';
+
 import './App.css';
+import CounterApp from './CounterApp';
 
 function App() {
   return (
     <div className="App">
-     
+        <h1>Hola</h1>
+        <CounterApp></CounterApp>
     </div>
   );
 }
